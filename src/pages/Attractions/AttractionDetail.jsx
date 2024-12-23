00317b28d67329e46e14039fe4336e5bb5bb045d
bong123123 <bongcheonjy@naver.com>
2024-12-23 12:46:16 +0900

    .

--- src/pages/Attractions/AttractionDetail.jsx
+++ src/pages/Attractions/AttractionDetail.jsx
@@ -5,7 +5,9 @@ import axios from 'axios';
 const AttractionDetail = () => {
   const { locationId } = useParams();  // URL에서 locationId 파라미터를 받아옴
   const [location, setLocation] = useState(null);
+  const [nearByLocations,setNearByLocations] = useState([])
   const [loading, setLoading] = useState(true);
+
 
   useEffect(() => {
     const fetchLocationDetail = async () => {
@@ -13,6 +15,21 @@ const AttractionDetail = () => {
         const response = await axios.get(`http://localhost:5050/api/locations/${locationId}`);
         setLocation(response.data);  // 상세 정보 저장
         console.log(response);
+                // 상세 정보에서 위도와 경도를 가져옴
+        const { latitude, longitude } = response.data;
+
+        // 근처 장소 정보 가져오기
+        const nearByResponse = await axios.get('http://localhost:5050/api/locations/getNearby', {
+          params: {
+            latitude,
+            longitude,
+            distance: 5, // 반경 5km
+            sortValue: 'googleRating', // 평점 순 정렬
+            sortDirection: 'desc', // 내림차순
+          },
+        });
+        setNearByLocations(nearByResponse.data);
+        console.log(nearByResponse)
       } catch (error) {
         console.error('Error fetching location details:', error);
       } finally {
@@ -43,6 +60,21 @@ const AttractionDetail = () => {
       <p>전화번호 : {location.phoneNumber}</p>
       <p>운영 시간 : {location.openingHours}</p>
       <p>웹사이트 주소 : <a href={location.website} target="_blank" rel="noopener noreferrer">{location.website}</a></p>
+
+      <h3>근처의 다른 장소</h3>
+      {nearByLocations.length > 0 ? (
+        <ul>
+          {nearByLocations.map((nearby) => (
+            <li key={nearby.id}>
+              <h4>{nearby.locationName}</h4>
+              <p>{nearby.formattedAddress}</p>
+              <p>평점: {nearby.googleRating}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>근처에 표시할 장소가 없습니다.</p>
+      )}
     </div>
   );
 };
